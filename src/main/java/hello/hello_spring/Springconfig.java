a4ebb088b7a49098f6a0946719bb208f73bd42fd
kilometer0107 <aearaear@naver.com>
2025-02-04 04:18:17 +0900

    spring study done

--- src/main/java/hello/hello_spring/Springconfig.java
+++ src/main/java/hello/hello_spring/Springconfig.java
@@ -1,21 +1,16 @@
 package hello.hello_spring;
-
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class Springconfig {
-
+    private final MemberRepository memberRepository;
+    public Springconfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new MemberService(memberRepository);
     }
 }
